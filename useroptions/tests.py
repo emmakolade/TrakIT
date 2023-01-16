ec029e33b30c581f3685753682ecdc9615119f45
Akolade <emmakolade@gmail.com>
2023-01-16 18:25:40 +0100

    __

--- useroptions/tests.py
+++ useroptions/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+# from django.test import TestCase
 
-# Create your tests here.
+# # Create your tests here.
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'zPMR1MTCeZLNkn3f5XOm',
+#         'HOST': 'containers-us-west-48.railway.app',
+#         'PORT': '6070'
+#     }
+# }
